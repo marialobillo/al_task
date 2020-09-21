5f210b28459879c62c46cf9e4b17cfb94e6bff83
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-09-21 17:33:11 +0200

    adding crypto module

--- controllers/index.js
+++ controllers/index.js
@@ -4,27 +4,26 @@ const crypto = require('crypto');
 
 const createValue = async (req, res) => {
   try {
-        const {id_value, encryption_key, value} = req.body;
+        const {id_service, encryption_key, value} = req.body;
 
        
           
-        const id_hashed = crypto.createHash('md5').update(id_value).digest('hex');
+        const id_hashed = crypto.createHash('md5').update(id_service).digest('hex');
         const key_hashed = crypto.createHash('md5').update(encryption_key).digest('hex');
         const value_hashed = crypto.createHash('md5').update(value).digest('hex');
-
 
         console.log("id: ", id_hashed);
         console.log("key: ", key_hashed);
         console.log("value: ", value_hashed);
 
-        const valueRow = await Values.create({
-            id_value: id_hashed, 
+        const service = await models.Service.create({
+            id_service: id_hashed, 
             encryption_key: key_hashed, 
             value: value_hashed
         });
-        console.log('The value created', valueRow.toJSON());
+        console.log('The value created', service.toJSON());
     return res.status(201).json({
-      valueRow
+      service
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
@@ -35,7 +34,7 @@ const getValueById = async (req, res) => {
     try {
       const { id_value, encryption_key } = req.body;
 
-      const value = await models.Values.findOne({
+      const value = await models.Services.findOne({
         where: { id_value: id_value, encryption_key: encryption_key },
       });
       
