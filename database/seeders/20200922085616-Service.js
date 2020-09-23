8eca22b3e88df1ad1db314f4c8668f62eb1af1ba
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-09-23 18:04:21 +0200

    service seeder

--- database/seeders/20200922085616-Service.js
+++ database/seeders/20200922085616-Service.js
@@ -4,13 +4,13 @@ module.exports = {
     'Services',
     [
       {
-        id: 'engineering-jobs-*',
+        id: 'engineering-jobs',
         value: JSON.stringify({ firstName: "Jane", lastName: "Doe" }),
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        id: 'marketing-jobs-*',
+        id: 'marketing-jobs',
         value: JSON.stringify({ firstName: "Jon", lastName: "Doe" }),
         createdAt: new Date(),
         updatedAt: new Date(),
